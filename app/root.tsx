37c7ee7992b77834850555035a2f6a0f12373c6e
Buck Brady <bbrady145@gmail.com>
2024-12-16 23:37:25 -0700

    added chakra-ui

--- app/root.tsx
+++ app/root.tsx
@@ -1,35 +1,34 @@
+import { withEmotionCache } from "@emotion/react"
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+} from "@remix-run/react"
+// import type { LinksFunction } from "@remix-run/node"
+import { ThemeProvider } from "next-themes"
+import { ChakraProvider } from "@/components/chakra-provider"
+import { withSentry } from "@sentry/remix"
+// import "./tailwind.css";
 
-import "./tailwind.css";
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+interface LayoutProps extends React.PropsWithChildren {}
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const Layout = withEmotionCache((props: LayoutProps, cache: any) => {
+  const { children } = props
+
   return (
     <html lang="en">
-      <head>
+      <head suppressHydrationWarning>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <meta
+          name="emotion-insertion-point"
+          content="emotion-insertion-point"
+        />
       </head>
       <body>
         {children}
@@ -37,9 +36,17 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
+})
+
+function App() {
+  return (
+    <ChakraProvider>
+      <ThemeProvider disableTransitionOnChange attribute="class">
+        <Outlet />
+      </ThemeProvider>
+    </ChakraProvider>
+  )
 }
 
-export default function App() {
-  return <Outlet />;
-}
+export default withSentry(App)
